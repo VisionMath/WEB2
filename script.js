5ffbb4e35e398fcbb770fb234c1944625f1b800a
VisionMath <103171106+VisionMath@users.noreply.github.com>
2022-04-14 16:30:26 +0900

    final?

--- script.js
+++ script.js
@@ -5,5 +5,13 @@ $(function () {
     $("#sidebar").load("sidebar.html");
 });
 $(function () {
+    $("#navbar").load("navbar.html");
+});
+$(function () {
     $("#footer").load("footer.html");
-}); 
+});
+function height_equal() {
+    article_height = $('#article').outerHeight();
+    $('.sidebar-container').css('min-height', article_height);
+    console.log(article_height);
+}
