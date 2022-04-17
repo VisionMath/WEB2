22b700c2abe60028f00b3d6c92f087098fbeaf87
DESKTOP-DH3Q88G\Panorama <blood337@naver.com>
2022-04-17 21:56:06 +0900

    sparkle & sakura

--- script.js
+++ script.js
@@ -10,8 +10,55 @@ $(function () {
 $(function () {
     $("#footer").load("footer.html");
 });
-function height_equal() {
-    article_height = $('#article').outerHeight();
-    $('.sidebar-container').css('min-height', article_height);
-    console.log(article_height);
+
+function night_day_handler(self) {
+    $(document).ready(function () {
+        if (self.value == 'day') {            
+            Tag.swap_color('body');
+            Tag.set_color('a, h1', 'aquamarine');
+            self.value = 'night';                    
+
+            $("body").sparkle({
+                color: [
+                    "#2eafea", "#e56604"
+                ],
+                speed: 0.4
+            });
+        } else {            
+            Tag.swap_color('body');
+            Tag.set_color('a, h1', 'pink');
+            self.value = 'day';            
+
+            var sakura = new Sakura('body', {
+                colors: [
+                    {
+                        gradientColorStart: 'rgba(255, 183, 197, 0.9)',
+                        gradientColorEnd: 'rgba(255, 197, 208, 0.9)',
+                        gradientColorDegree: 120
+                    }, {
+                        gradientColorStart: 'rgba(255,189,189)',
+                        gradientColorEnd: 'rgba(227,170,181)',
+                        gradientColorDegree: 120
+                    }, {
+                        gradientColorStart: 'rgba(212,152,163)',
+                        gradientColorEnd: 'rgba(242,185,196)',
+                        gradientColorDegree: 120
+                    }
+                ],
+                delay: 200
+            });
+        }
+    });
+}
+
+var Tag = {
+    swap_color: function (tag) {
+        var color = $(tag).css('color');
+        var bgcolor = $(tag).css('background-color');
+        $(tag).css('color', bgcolor);
+        $(tag).css('background-color', color);
+    },
+    set_color: function (tag, color) {
+        $(tag).css('color', color);
+    }
 }
