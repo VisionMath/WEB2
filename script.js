cc791e4bae5443117cb571f1036a60f2559ffe9a
VisionMath <103171106+VisionMath@users.noreply.github.com>
2022-04-18 16:10:56 +0900

    ani on off

--- script.js
+++ script.js
@@ -13,40 +13,26 @@ $(function () {
 
 function night_day_handler(self) {
     $(document).ready(function () {
-        if (self.value == 'day') {            
+        $("body").sparkle({
+            color: [
+                "#2eafea", "#e56604"
+            ],
+            speed: 0.4
+        });
+        $("body").off("mouseover.sparkle");
+        $("body").off("mouseout.sparkle");
+        $("body").off("focus.sparkle");
+        $("body").off("blur.sparkle");
+        if (self.value == 'day') {
             Tag.swap_color('body');
             Tag.set_color('a, h1', 'aquamarine');
-            self.value = 'night';                    
-
-            $("body").sparkle({
-                color: [
-                    "#2eafea", "#e56604"
-                ],
-                speed: 0.4
-            });
-        } else {            
+            self.value = 'night';
+            $("body").trigger("start.sparkle");
+        } else {
             Tag.swap_color('body');
             Tag.set_color('a, h1', 'pink');
-            self.value = 'day';            
-
-            var sakura = new Sakura('body', {
-                colors: [
-                    {
-                        gradientColorStart: 'rgba(255, 183, 197, 0.9)',
-                        gradientColorEnd: 'rgba(255, 197, 208, 0.9)',
-                        gradientColorDegree: 120
-                    }, {
-                        gradientColorStart: 'rgba(255,189,189)',
-                        gradientColorEnd: 'rgba(227,170,181)',
-                        gradientColorDegree: 120
-                    }, {
-                        gradientColorStart: 'rgba(212,152,163)',
-                        gradientColorEnd: 'rgba(242,185,196)',
-                        gradientColorDegree: 120
-                    }
-                ],
-                delay: 200
-            });
+            self.value = 'day';
+            $("body").trigger("stop.sparkle");
         }
     });
 }
