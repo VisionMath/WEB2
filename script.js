95f9eaaa10b5d896c7678c639a70b30d2c8138e7
visionmath <blood337@naver.com>
2022-04-18 16:07:05 +0900

    ani on off

--- script.js
+++ script.js
@@ -10,8 +10,41 @@ $(function () {
 $(function () {
     $("#footer").load("footer.html");
 });
-function height_equal() {
-    article_height = $('#article').outerHeight();
-    $('.sidebar-container').css('min-height', article_height);
-    console.log(article_height);
+
+function night_day_handler(self) {
+    $(document).ready(function () {
+        $("body").sparkle({
+            color: [
+                "#2eafea", "#e56604"
+            ],
+            speed: 0.4
+        });
+        $("body").off("mouseover.sparkle");
+        $("body").off("mouseout.sparkle");
+        $("body").off("focus.sparkle");
+        $("body").off("blur.sparkle");
+        if (self.value == 'day') {
+            Tag.swap_color('body');
+            Tag.set_color('a, h1', 'aquamarine');
+            self.value = 'night';
+            $("body").trigger("start.sparkle");
+        } else {
+            Tag.swap_color('body');
+            Tag.set_color('a, h1', 'pink');
+            self.value = 'day';
+            $("body").trigger("stop.sparkle");
+        }
+    });
+}
+
+var Tag = {
+    swap_color: function (tag) {
+        var color = $(tag).css('color');
+        var bgcolor = $(tag).css('background-color');
+        $(tag).css('color', bgcolor);
+        $(tag).css('background-color', color);
+    },
+    set_color: function (tag, color) {
+        $(tag).css('color', color);
+    }
 }
